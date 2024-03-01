43a105c0ed6018118c94c4d7552d25879d570410
JohnnyG1234 <89805746+JohnnyG1234@users.noreply.github.com>
2024-03-01 11:45:43 -0500

    Finishing up box formating

--- DMScreenReactProject/src/App.tsx
+++ DMScreenReactProject/src/App.tsx
@@ -8,12 +8,12 @@ function App() {
   return (
     <div className="GridWrap">
         <PageHeaderBox/>
-        <Box classString="b square"></Box>
-        <Box classString="c square"></Box>
-        <Box classString="d square"></Box>
-        <Box classString="e square"></Box>
-        <Box classString="f square"></Box>
-        <Box classString="g square"></Box>
+        <Box textContent="Condition Rules" classString="b square"></Box>
+        <Box textContent="DC Guide" classString="c square"></Box>
+        <Box textContent="Rabdom Tavern" classString="d square"></Box>
+        <Box textContent="Random Encounter" classString="e square"></Box>
+        <Box textContent="Random Loot Table" classString="f square"></Box>
+        <Box textContent="Random NPC" classString="g square"></Box>
     </div>
   );
 }
--- DMScreenReactProject/src/box.tsx
+++ DMScreenReactProject/src/box.tsx
@@ -3,11 +3,12 @@ import './App.css';
 
 interface Props {
     classString: string;
+    textContent: string;
   }
 
 function Box(props: Props){
     return (
-        <div className={props.classString}> Box </div>
+        <div className={props.classString}> {props.textContent} </div>
     );
 }
 
