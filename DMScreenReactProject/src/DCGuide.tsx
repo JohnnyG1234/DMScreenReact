7ada9635c6798df9fe61ee3edc6cd12bbb8746e0
John <john.groton@mymail.champlain.edu>
2024-03-05 15:34:55 -0500

    finishing up dc guide

--- DMScreenReactProject/src/DCGuide.tsx
+++ DMScreenReactProject/src/DCGuide.tsx
@@ -6,7 +6,45 @@ function DCGuide(){
         <> 
         <div className="c square">
           <h2>DC Guide</h2>
-        </div>
+          <table className='table'>
+            <tr>
+              <th>Task Difficulty</th>
+              <th>DC</th>
+            </tr>
+
+            <div className='tableLine'></div>
+            
+            <tr>
+              <th>Very Easy</th>
+              <th>5</th>
+            </tr>
+
+            <tr>
+              <th>Easy</th>
+              <th>10</th>
+            </tr>
+
+            <tr>
+              <th>Medium</th>
+              <th>15</th>
+            </tr>
+
+            <tr>
+              <th>Hard</th>
+              <th>20</th>
+            </tr>
+
+            <tr>
+              <th>Very Hard</th>
+              <th>25</th>
+            </tr>
+
+            <tr>
+              <th>Nearly Impossible</th>
+              <th>30</th>
+            </tr>
+            </table>
+          </div>  
         </>
     );
 }
