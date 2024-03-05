3d218d146b17d07a0c2918168d985c008e324aae
JohnnyG1234 <89805746+JohnnyG1234@users.noreply.github.com>
2024-03-05 12:44:05 -0500

    Starting work on DC Guide

---
+++ DMScreenReactProject/src/DCGuide.tsx
@@ -0,0 +1,14 @@
+import { Children } from 'react';
+import './App.css';
+
+function DCGuide(){
+    return (
+        <> 
+        <div className="c square">
+          <h2>DC Guide</h2>
+        </div>
+        </>
+    );
+}
+
+export default DCGuide;
